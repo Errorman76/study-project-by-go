e61d42fa64c0a00665e08b3c0a84ed5370edbf1d
Errorman76 <lwooyoung57@gmail.com>
2023-09-07 22:23:19 +0900

    feat: go method add

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"my-go-app/example/hello"
-
-	"myFunc.com/ufunc"
+	"fmt"
+	"my-go-app/example/accounts"
+	// "my-go-app/example/hello"
+	// "myFunc.com/ufunc"
 )
 
 func main() {
-	hello.PrintHello()
-	ufunc.PrintFunc()
+	// hello.PrintHello()
+	// ufunc.PrintFunc()
+
+	account := accounts.NewAccount("nicolas")
+	account.Deposit(10)
+	println(*account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	println(*account.Balance())
 }

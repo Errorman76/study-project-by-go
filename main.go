2f37b78984ee9db035eecdf042ca50a6142a267d
Errorman76 <lwooyoung57@gmail.com>
2023-09-11 23:18:07 +0900

    feat: url checker start

--- main.go
+++ main.go
@@ -1,22 +1,51 @@
+// package main
+
+// func main() {
+// 	hello.PrintHello()
+// 	ufunc.PrintFunc()
+
+// 	account := accounts.NewAccount("nicolas")
+// 	account.Deposit(10)
+// 	println(*account.Balance())
+// 	err := account.Withdraw(20)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	println(*account.Balance(), account.Owner())
+// }
+
 package main
 
 import (
+	"errors"
 	"fmt"
-	"my-go-app/example/accounts"
-	// "my-go-app/example/hello"
-	// "myFunc.com/ufunc"
+	"net/http"
 )
 
-func main() {
-	// hello.PrintHello()
-	// ufunc.PrintFunc()
+var errRequestFailed = errors.New("Request failed")
 
-	account := accounts.NewAccount("nicolas")
-	account.Deposit(10)
-	println(*account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	println(*account.Balance(), account.Owner())
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
